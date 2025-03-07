6e333f8828b615d4bbb5462c37a7d2b9f549faf5
ShadowNinja909 <175156558+TerrellAW@users.noreply.github.com>
2025-03-06 22:01:35 -0700

    Added selection boxes to trending page

--- screens/TrendingScreen.js
+++ screens/TrendingScreen.js
@@ -7,6 +7,23 @@ export default function TrendingScreen() {
       {/* Header */}
       <View style={styles.header}>
         <Text style={styles.title}>🔥 Trending Screen</Text>
+        {/* Search bar and selection boxes with trending selected, blue text and border bottom */}
+        <View style={styles.selectionBoxes}>
+          <View style={styles.selectionBox}>
+            <Text style={styles.selectionText}>For You</Text>
+          </View>
+          <View style={[styles.selectionBox, styles.selected]}>
+            <Text style={[styles.selectionText, styles.selectedText]}>
+              Trending
+            </Text>
+          </View>
+          <View style={styles.selectionBox}>
+            <Text style={styles.selectionText}>News</Text>
+          </View>
+          <View style={styles.selectionBox}>
+            <Text style={styles.selectionText}>Sports</Text>
+          </View>
+        </View>
       </View>
       {/* Main Content */}
       <ScrollView></ScrollView>
@@ -22,16 +39,38 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   header: {
-    paddingVertical: 20,
     paddingTop: 40,
     backgroundColor: "#fff",
     width: "100%",
     alignItems: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  selectionText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#787878",
+    paddingBottom: 20,
+  },
+  selectedText: {
+    color: "#007AFF",
+  },
+  selectionBoxes: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingTop: 10,
+    marginTop: 10,
+  },
+  selectionBox: {
+    flex: 1,
+    alignItems: "center",
+    borderBottomWidth: 2,
+    borderBottomColor: "#ccc",
+  },
+  selected: {
+    borderBottomColor: "#007AFF",
   },
 });
